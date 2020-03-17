ec8d9a8f594b1eb215a1beb913f87ea8ffa26d35
Blagovest Ivanov <35682972+BlagovestIvanov@users.noreply.github.com>
2020-03-17 19:11:22 +0200

    Add files via upload

---
+++ dbo.AspNetRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'7177285d-d636-4255-9daf-21fe16e56027', N'CanManageMovies')
---
+++ dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'c7de9e1d-df80-4221-900b-6f3bebe2c415', N'7177285d-d636-4255-9daf-21fe16e56027')
